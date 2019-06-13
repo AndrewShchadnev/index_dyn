fea94255cc36354a879bf92a71208414b803c70a
Andrew Shchadnev <a.shchadnev@yandex.ru>
2019-06-13 15:37:51 +0300

    Delete model.py

--- model.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import *
-
-metadata = MetaData()
-
-md5 = Table('md5', metadata,
-            Column('MSISDN', BigInteger, nullable=False, primary_key=True),
-            Column('MD5', String(32), unique=True)
-            )
